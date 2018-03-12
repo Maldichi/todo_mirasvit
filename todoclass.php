8e78e4603a7149b0e502625d14379e7fdfae7b20
Maldichi <yarmusevich@ukr.net>
2018-03-12 12:49:02 +0200

    Refresh

--- todoclass.php
+++ todoclass.php
@@ -4,15 +4,15 @@
 <body>
 <p>
     <?php
-
-
-    class Repository {
+    class Task {
         protected $server = '127.0.0.1';
         protected $user = 'root';
         protected $pass = 'astral';
         protected $data = 'todo_base';
 
-        public function connect() {
+        protected $connection;
+
+        public function __construct() {
 
             $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
 
@@ -20,78 +20,87 @@
                 printf("Connection failed: %s\/", mysqli_connect_error());
                 exit();
             }
-
-            return true;
         }
-        public function listing () {
-            $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
+        public function Subj($subject){
+            return $this->subject = $subject;
+        }
+    }
+
+    class Repository extends Task
+    {
+
+        function List()
+        {
+            $connection = new mysqli($this->server, $this->user, $this->pass, $this->data);
             $res = "SELECT * FROM tasks";
             $result = $connection->query($res);
-            $len =  $result->num_rows;
-
-            for ($i=0;$i<$len; $i++) {
-                $row = $result->fetch_array(MYSQLI_ASSOC);
-                echo '# of task '. $row['id'].'Name of task '. $row['name'].'<br>';
-            }
-
+            return $result;
         }
-
-//        public function getId()
+//        public function getId($id)
 //        {
 //            $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
-//            $id = "SELECT id FROM tasks";
+//            $id = "SELECT '$id' FROM tasks";
 //            $result = $connection->query($id);
 //            $row = $result->fetch_array(MYSQLI_ASSOC);
-//
-//
 //        }
-        public function saveTask($subject) {
-            $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
+//        public function delTask ($id) {
+//            $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
+//            if (isset($_POST['del_submit'])) {
+//                if(!empty($id)){
+//                    $id = "DELETE FROM tasks WHERE id = $id";
+//                    $connection->query($id);
+//                }
+//            }
+//        }
+
+        public function Save($subject)
+        {
+
+            $connection = new mysqli($this->server, $this->user, $this->pass, $this->data);
+
             if (isset($_POST['submit'])) {
-                if(!empty($subject)){
+                if (!empty($subject)) {
                     $sql = "INSERT INTO tasks (name) VALUES ('$subject')";
                     $connection->query($sql);
                 }
-                else {
-                   echo 'empty field';
-                }
+
             }
         }
-    }
 
-
-
-
-    class Task {
-        public $id;
-        public $subject;
-
-        public function setId($id, $subject)
+        public function New()
         {
-            $this->id = $id;
-            $this->subject = $subject;
+            return new Task();
         }
     }
-    $task = new Task();
 
     $con = new Repository();
-    $con->connect();
-    $con->getId();
+    $tasks = $con->New();
+    $task = $tasks->Subj($_POST['task']);
+    $con->Save($task);
+    $list = $con->List();
     ?>
+
+
 
     <h1>Todo application</h1>
 <form method="post" class="form">
 
-    <label for="">Name of task</label>
+    <label>Name of task</label>
     <input type="text" name="task" class="task">
 
-<button type="submit" name="submit" id="add_task">Add Task</button>
+<button type="submit" name="submit" id="submit">Add Task</button>
 
 </form>
-<p><?php
-$con->saveTask($_POST['task']);?></p>
+
 <?php
-$con->listing();
+
+foreach ($list as $key => $value) {
+
+        echo 'Tasks # '. $value['id']. 'Tasks name:' .$value['name'].'<br>';
+
+}
+
+
 ?>
 </p>
 </body>
