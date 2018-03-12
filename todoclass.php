512449bdf1112d8d26a4ddfeef428fa670d10a0f
Maldichi <yarmusevich@ukr.net>
2018-03-12 16:31:36 +0200

    Update todoclass.php

--- todoclass.php
+++ todoclass.php
@@ -5,98 +5,77 @@
 <p>
     <?php
     class Task {
-
         public function Subj($subject){
             return $this->subject = $subject;
         }
     }
-
     class DB{
-    protected $server = '127.0.0.1';
+        protected $server = '127.0.0.1';
         protected $user = 'root';
         protected $pass = 'astral';
         protected $data = 'todo_base';
-
         protected $connection;
-
         public function __construct() {
-
             $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
-
             if ( mysqli_connect_errno() ) {
                 printf("Connection failed: %s\/", mysqli_connect_error());
                 exit();
             }
-            }
-        public function Get() {
+        }
+    }
+
+
+    class Repository extends DB
+    {
+        function List()
+        {
             $connection = new mysqli( $this->server, $this->user, $this->pass, $this->data );
             $res = "SELECT * FROM tasks";
             $result = $connection->query($res);
             return $result;
         }
-        public function Saved($subject){
-            $connection = new mysqli($this->server, $this->user, $this->pass, $this->data);
-
-            if (isset($_POST['submit'])) {
-                if (!empty($subject)) {
-                    $sql = "INSERT INTO tasks (name) VALUES ('$subject')";
-                    $connection->query($sql);
-                }
-
-            }
-        }
-
-    }
-    
-    
-    class Repository extends DB
-    {
-        function List()
-        {
-            return $this->Get();
-        }
-
         public function Save($subject)
         {
-            $this->Saved($subject);
+            $connection = new mysqli($this->server, $this->user, $this->pass, $this->data);
+            $sql = "INSERT INTO tasks (name) VALUES ('$subject')";
+            $connection->query($sql);
         }
-
         public function New()
         {
             return new Task();
         }
     }
-    
-    
+
 
     $con = new Repository();
     $tasks = $con->New();
     $task = $tasks->Subj($_POST['task']);
-    $con->Save($task);
+
     $list = $con->List();
+
+    if (isset($_POST['submit'])) {
+        if (!empty($task)) {
+            $con->Save($task);
+        }
+    }
     ?>
 
 
 
-    <h1>Todo application</h1>
+<h1>Todo application</h1>
 <form method="post" class="form">
 
     <label>Name of task</label>
     <input type="text" name="task" class="task">
 
-<button type="submit" name="submit" id="submit">Add Task</button>
+    <button type="submit" name="submit" id="submit">Add Task</button>
 
 </form>
 
 <?php
-
 foreach ($list as $key => $value) {
-
-        echo 'Tasks # '. $value['id']. 'Tasks name:' .$value['name'].'<br>';
-
+    echo 'Tasks # '. $value['id']. 'Tasks name:' .$value['name'].'<br>';
 }
-
-
 ?>
 </p>
 </body>
